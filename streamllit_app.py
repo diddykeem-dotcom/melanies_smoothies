fcaec208756e7ff072111556683484c8e264df5b
diddykeem-dotcom <diddykeem@gmail.com>
2025-12-26 09:53:05 +0900

    Refactor session handling in streamlit_app.py

--- streamllit_app.py
+++ streamllit_app.py
@@ -12,7 +12,6 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The Name on your Smoothie will be:", name_on_order)
 
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -35,6 +34,9 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
         values ('""" + ingredients_string + """','"""+ name_on_order+"""')"""
 
+    cnx = st.connection("snowflake")
+    session = cnx.session()
+  
     st.write(my_insert_stmt)
     # st.stop()
 
